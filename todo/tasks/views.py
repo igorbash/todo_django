7742361bf5f78e091d427981b9a1e94b280a874d
igor <igor731996@gmail.com>
2020-09-26 16:23:18 +0300

    Change view to class view

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from django.http import HttpResponse
 
 from .models import *
 from .forms import *
 
 
-def index(request):
-    tasks = Task.objects.all()
+class IndexView(View):
+    def get(self, request):
+        tasks = Task.objects.all()
+        tasks_form = TaskForm()
 
-    tasks_form = TaskForm()
+        context = {'tasks': tasks, 'tasks_form': tasks_form}
+        return render(request, 'tasks/index.html', context)
 
-    if request.method == 'POST':
+    def post(self, request):
         tasks_form = TaskForm(request.POST)
         if tasks_form.is_valid():
             tasks_form.save()
         return redirect('/')
-
-    context = {'tasks': tasks, 'tasks_form': tasks_form}
-    return render(request, 'tasks/index.html', context)
