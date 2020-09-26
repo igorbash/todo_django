8ba258e503a90a76916c9fe4238223aec4964390
igor <igor731996@gmail.com>
2020-09-26 16:41:14 +0300

    Add update to task

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view())
+    path('', views.IndexView.as_view(), name='index'),
+    path('update_task/<str:primary_key>/', views.UpdateTask.as_view(), name='update_task')
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -7,15 +7,35 @@ from .forms import *
 
 
 class IndexView(View):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         tasks = Task.objects.all()
         tasks_form = TaskForm()
 
         context = {'tasks': tasks, 'tasks_form': tasks_form}
         return render(request, 'tasks/index.html', context)
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
         tasks_form = TaskForm(request.POST)
         if tasks_form.is_valid():
             tasks_form.save()
         return redirect('/')
+
+
+class UpdateTask(View):
+    @staticmethod
+    def get(request, primary_key):
+        task = Task.objects.get(id=primary_key)
+        task_form = TaskForm(instance=task)
+
+        context = {'form': task_form}
+        return render(request, 'tasks/update_task.html', context)
+
+    @staticmethod
+    def post(request, primary_key):
+        task = Task.objects.get(id=primary_key)
+        task_form = TaskForm(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('/')
