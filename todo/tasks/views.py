53e8e14bc1c38471a70544f5ab4b88b37cb2f48f
igor <igor731996@gmail.com>
2020-09-26 16:15:27 +0300

    Can now add tasks

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks': tasks}
-    return render(request, 'tasks/list.html', context)
+    tasks_form = TaskForm()
+
+    if request.method == 'POST':
+        tasks_form = TaskForm(request.POST)
+        if tasks_form.is_valid():
+            tasks_form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'tasks_form': tasks_form}
+    return render(request, 'tasks/index.html', context)
